579607791e75025a4016de832d4d9f209eadf88c
Michael Threels <michaelthreels@gmail.com>
2020-12-08 23:33:38 +0100

    feat: added styling for driver details page

--- src/components/driver-details.js
+++ src/components/driver-details.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable no-restricted-globals */
 import React from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import detailStyles from './styles/driver-details.module.css';
 
 const driverDetails = props => {
   const { scores } = props;
@@ -10,53 +12,55 @@ const driverDetails = props => {
   const { driver } = useParams();
   const currentDriver = scores.find(c => c.position === driver);
   return (
-    <div>
-      <h1>
-        {currentDriver.Driver.code}
-      </h1>
-      <h2>
-        {currentDriver.Driver.givenName}
-        &nbsp;
-        {currentDriver.Driver.familyName}
-      </h2>
-      <p>
-        Position in championship:
-        &nbsp;
-        {currentDriver.position}
-      </p>
-      <p>
-        Number of points:
-        &nbsp;
-        {currentDriver.points}
-      </p>
-      <p>
-        Number of wins:
-        &nbsp;
-        {currentDriver.wins}
-      </p>
-      <p>
-        Constructor:
-        &nbsp;
-        <a href={currentDriver.Constructors[0].url}>{currentDriver.Constructors[0].name}</a>
-      </p>
-      <p>
-        Nationality:
-        &nbsp;
-        {currentDriver.Driver.nationality}
-      </p>
-      <p>
-        Permanent number:
-        &nbsp;
-        {currentDriver.Driver.permanentNumber}
-      </p>
-      <p>
-        Date of birth:
-        &nbsp;
-        {currentDriver.Driver.dateOfBirth}
-      </p>
-      <p>
-        <a href={currentDriver.Driver.url}>more...</a>
-      </p>
+    <div className={detailStyles.container}>
+      <div className={detailStyles.innerContainer}>
+        <h1>
+          {currentDriver.Driver.code}
+        </h1>
+        <h2>
+          {currentDriver.Driver.givenName}
+          &nbsp;
+          {currentDriver.Driver.familyName}
+        </h2>
+        <p>
+          Position in championship:
+          &nbsp;
+          {currentDriver.position}
+        </p>
+        <p>
+          Number of points:
+          &nbsp;
+          {currentDriver.points}
+        </p>
+        <p>
+          Number of wins:
+          &nbsp;
+          {currentDriver.wins}
+        </p>
+        <p>
+          Constructor:
+          &nbsp;
+          <a href={currentDriver.Constructors[0].url} target="_blank">{currentDriver.Constructors[0].name}</a>
+        </p>
+        <p>
+          Nationality:
+          &nbsp;
+          {currentDriver.Driver.nationality}
+        </p>
+        <p>
+          Permanent number:
+          &nbsp;
+          {currentDriver.Driver.permanentNumber}
+        </p>
+        <p>
+          Date of birth:
+          &nbsp;
+          {currentDriver.Driver.dateOfBirth}
+        </p>
+        <p>
+          <a href={currentDriver.Driver.url} target="_blank">more...</a>
+        </p>
+      </div>
     </div>
   );
 };
